e293805b8818352aedf9f9cb4c2cd1dfac425520
MutisoStud02 <mutiso8395@gmail.com>
2025-04-01 15:37:33 +0300

    Created state initialized to 0 that will be used to manipulate index of lifeQuotes array Created functions to move to next and previous lifeQuotes on the screen Created logic to disable buttons when previous button is clicked while the first quote is displayed Created logic to disable next button when last item is on the screen Created buttons that contain the previous and next click logics Created a button with Restore icon to reset screen to show the first quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react";
+import Quotes from "./Components/Quotes";
+import lifeQuotes from "./utilities";
+import Icon from "@mdi/react";
+import { mdiRestore } from "@mdi/js";
+
 function App() {
+  const [quoteIndex, setQuoteIndex] = useState(0);
+  console.log(lifeQuotes.length)
+  function handleNextClick() {
+    if (quoteIndex < lifeQuotes.length - 1) {
+      setQuoteIndex(quoteIndex + 1);
+    }
+  }
+  function handlePrevClick() {
+    if (quoteIndex > 0) {
+      setQuoteIndex(quoteIndex - 1);
+    }
+  }
+  function handleReset() {
+    setQuoteIndex(0);
+  }
   return (
-    <h1>STOCK CONTROL APP</h1>
-  )
+    <>
+      <h1>LIFE QUOTES APP</h1>
+      <div className="content">
+        <div className="body">
+          <button disabled={quoteIndex === 0} onClick={handlePrevClick} className="slide-btns">
+            Prev
+          </button>
+          <Quotes quote={quoteIndex} />
+          <button
+            disabled={quoteIndex === lifeQuotes.length - 1}
+            onClick={handleNextClick}
+            className="slide-btns"
+          >
+            Next
+          </button>
+        </div>
+        <button className="reset" onClick={handleReset}>
+          <Icon path={mdiRestore} title="Restore" size={1.6} color="black" />
+        </button>
+      </div>
+    </>
+  );
 }
 export default App;
