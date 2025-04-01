cfefd92945992fe3cca3994e645b946c793a0fc6
MutisoStud02 <mutiso8395@gmail.com>
2025-04-01 15:34:55 +0300

    Created a lifeQuotes array of objects with quotes and their owners

---
+++ src/utilities.js
@@ -0,0 +1,329 @@
+const lifeQuotes = [
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship.",
+    "quotedBy": "Buddha"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.",
+    "quotedBy": "Dr. Seuss"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life would be tragic if it weren’t funny.",
+    "quotedBy": "Stephen Hawking"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Live in the sunshine, swim the sea, drink the wild air.",
+    "quotedBy": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life is what we make it, always has been, always will be.",
+    "quotedBy": "Grandma Moses"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life’s tragedy is that we get old too soon and wise too late.",
+    "quotedBy": "Benjamin Franklin"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life is about making an impact, not making an income.",
+    "quotedBy": "Kevin Kruse"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life is short, and it is here to be lived.",
+    "quotedBy": "Kate Winslet"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The longer I live, the more beautiful life becomes.",
+    "quotedBy": "Frank Lloyd Wright"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Every moment is a fresh beginning.",
+    "quotedBy": "T.S. Eliot"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "When you cease to dream you cease to live.",
+    "quotedBy": "Malcolm Forbes"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "If you spend your whole life waiting for the storm, you'll never enjoy the sunshine.",
+    "quotedBy": "Morris West"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Don't cry because it's over, smile because it happened.",
+    "quotedBy": "Dr. Seuss"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life comes from the earth and life returns to the earth.",
+    "quotedBy": "Zhuangzi"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "We are here to add what we can to life, not to get what we can from life.",
+    "quotedBy": "William Osler"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The art of life is to know how to enjoy a little and to endure very much.",
+    "quotedBy": "William Hazlitt"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "If you change the way you look at things, the things you look at change.",
+    "quotedBy": "Wayne Dyer"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Believe you can and you're halfway there.",
+    "quotedBy": "Theodore Roosevelt"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life can only be understood backwards; but it must be lived forwards.",
+    "quotedBy": "Søren Kierkegaard"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life is a song - sing it. Life is a game - play it. Life is a challenge - meet it. Life is a dream - realize it. Life is a sacrifice - offer it. Life is love - enjoy.",
+    "quotedBy": "Sai Baba"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.",
+    "quotedBy": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "It is not length of life, but depth of life.",
+    "quotedBy": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing.",
+    "quotedBy": "George Bernard Shaw"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life is really simple, but we insist on making it complicated.",
+    "quotedBy": "Confucius"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The purpose of life is to believe, to hope, and to strive.",
+    "quotedBy": "Indira Gandhi"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Our lives begin to end the day we become silent about things that matter.",
+    "quotedBy": "Martin Luther King Jr."
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+    "quotedBy": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "In three words I can sum up everything I've learned about life: it goes on.",
+    "quotedBy": "Robert Frost"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life is 10% what happens to us and 90% how we react to it.",
+    "quotedBy": "Charles R. Swindoll"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The biggest adventure you can take is to live the life of your dreams.",
+    "quotedBy": "Oprah Winfrey"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Good friends, good books, and a sleepy conscience: this is the ideal life.",
+    "quotedBy": "Mark Twain"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The best way to predict your future is to create it.",
+    "quotedBy": "Abraham Lincoln"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "You only live once, but if you do it right, once is enough.",
+    "quotedBy": "Mae West"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life isn't about finding yourself. Life is about creating yourself.",
+    "quotedBy": "George Bernard Shaw"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    "quotedBy": "Albert Schweitzer"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life is not measured by the number of breaths we take, but by the moments that take our breath away.",
+    "quotedBy": "George Carlin"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The only impossible journey is the one you never begin.",
+    "quotedBy": "Tony Robbins"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+    "quotedBy": "Brian Tracy"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Do not wait; the time will never be 'just right'. Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along.",
+    "quotedBy": "George Herbert"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Success usually comes to those who are too busy to be looking for it.",
+    "quotedBy": "Henry David Thoreau"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "I find that the harder I work, the more luck I seem to have.",
+    "quotedBy": "Thomas Jefferson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+    "quotedBy": "Roy T. Bennett"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "In order to succeed, we must first believe that we can.",
+    "quotedBy": "Nikos Kazantzakis"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Life isn't about waiting for the storm to pass, it's about learning how to dance in the rain.",
+    "quotedBy": "Vivian Greene"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "It does not matter how slowly you go as long as you do not stop.",
+    "quotedBy": "Confucius"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Everything you’ve ever wanted is on the other side of fear.",
+    "quotedBy": "George Addair"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Do one thing every day that scares you.",
+    "quotedBy": "Eleanor Roosevelt"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The future belongs to those who believe in the beauty of their dreams.",
+    "quotedBy": "Eleanor Roosevelt"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Act as if what you do makes a difference. It does.",
+    "quotedBy": "William James"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "You don’t have to be great to start, but you have to start to be great.",
+    "quotedBy": "Zig Ziglar"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "quotedBy": "Winston Churchill"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    "quotedBy": "Christian D. Larson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Don't watch the clock; do what it does. Keep going.",
+    "quotedBy": "Sam Levenson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "If you want to live a happy life, tie it to a goal, not to people or things.",
+    "quotedBy": "Albert Einstein"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Limit your 'always' and your 'nevers.'",
+    "quotedBy": "Amy Poehler"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Everything you can imagine is real.",
+    "quotedBy": "Pablo Picasso"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "You are never too old to set another goal or to dream a new dream.",
+    "quotedBy": "C.S. Lewis"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "To live is the rarest thing in the world. Most people exist, that is all.",
+    "quotedBy": "Oscar Wilde"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The best time to plant a tree was 20 years ago. The second best time is now.",
+    "quotedBy": "Chinese Proverb"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+    "quotedBy": "Ralph Waldo Emerson"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "A person who never made a mistake never tried anything new.",
+    "quotedBy": "Albert Einstein"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The best way to get started is to quit talking and begin doing.",
+    "quotedBy": "Walt Disney"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "You don't have to be great to start, but you have to start to be great.",
+    "quotedBy": "Zig Ziglar"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "The only way to do great work is to love what you do.",
+    "quotedBy": "Steve Jobs"
+  },
+  {
+    "id": "crypto.randomUUID()",
+    "text": "Don't wait. The time will never be just right.",
+    "quotedBy": "Napoleon Hill"
+  }
+]
+
+  export default lifeQuotes;
